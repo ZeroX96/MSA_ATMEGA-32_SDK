6fc3043162ce2e4466fa7173c23ec9cbfc4856be
Mahmoud_Saad <msammamsa10@gmail.com>
2018-09-11 20:30:24 +0200

    SDK_USART-initial commit.

---
+++ HAL_layer/HAL_USART_DONE/HAL_USART_DONE/main.c
@@ -0,0 +1,18 @@
+/*
+ * HAL_USART_DONE.c
+ *
+ * Created: 11/09/2018 20:28:12
+ * Author : Mahmoud
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
