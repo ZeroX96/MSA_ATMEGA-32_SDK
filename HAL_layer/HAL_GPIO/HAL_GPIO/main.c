294dfc7b8b697e10242f265b82f1bb6ea9eed68c
Mahmoud_Saad <msammamsa10@gmail.com>
2018-09-11 20:58:07 +0200

    SDK_GPIO-initial commit.

---
+++ HAL_layer/HAL_GPIO/HAL_GPIO/main.c
@@ -0,0 +1,18 @@
+/*
+ * HAL_GPIO.c
+ *
+ * Created: 11/09/2018 20:53:03
+ * Author : Mahmoud
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
