e2c9577dc1a5b15765c259520079fc4626927c8b
Mahmoud_Saad <msammamsa10@gmail.com>
2018-09-18 16:13:38 +0200

    SDK_PWM-initial commit

---
+++ HAL_layer/HAL_PWM/HAL_PWM/main.c
@@ -0,0 +1,21 @@
+/*
+ * HAL_PWM.c
+ *
+ * Created: 12/09/2018 04:19:54
+ * Author : Mahmoud
+ */ 
+
+#include <avr/io.h>
+
+/*
+*will devide the slot/the available time period into slots so the motor will be controlled with +-10 every pwm_up or pwm_dwn
+*will work on the three available pwm modes specially with the one with the large period of time so we have more ctr over the motor speed
+*/
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
