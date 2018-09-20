6de5d626d3d36be12b933fa7515db718a48066eb
Mahmoud_Saad <msammamsa10@gmail.com>
2018-09-20 02:59:20 +0200

    the common.h pla pla pla

--- HAL_layer/HAL_USART/HAL_USART/common.h
+++ HAL_layer/HAL_USART/HAL_USART/common.h
@@ -15,11 +15,11 @@ typedef enum{
 }tenum_bool;
 
 #define NULL ((void *)0)
-typedef tenum_bool bool_t;
-typedef unsigned char msa_u8;
-typedef unsigned short int msa_u16;
-typedef unsigned long int msa_u32;
-typedef signed char msa_s8;
-typedef signed short int msa_s16;
-typedef signed long int msa_s32;
+typedef tenum_bool 			bool_t;
+typedef unsigned char 		msa_u8;
+typedef unsigned short int 	msa_u16;
+typedef unsigned long int 	msa_u32;
+typedef signed char 		msa_s8;
+typedef signed short int 	msa_s16;
+typedef signed long int 	msa_s32;
 #endif /* COMMON_H_ */
