2210f0f04f882a669ef12e0a585eddf5b4c735d0
Mahmoud_Saad <msammamsa10@gmail.com>
2018-10-28 06:44:37 +0200

    small changes :D

--- HAL_layer/HAL_USART/HAL_USART/main.c
+++ HAL_layer/HAL_USART/HAL_USART/main.c
@@ -17,35 +17,30 @@ int main(void)
 {
     /* Replace with your application code */
 
-	usart_init(&usart_1,B_SENDER_N_RECEIVER,ONE_STP_BIT,NO_PARITY,EIGHT,INTERRUPT_DRIVEN,FALLING_EDGE); 
-    //msa_u8 got_val;
-	//msa_u8 temp_val=' ';
-	//msa_u8 *arr_add=(msa_u8 *)"you gave me this =>";
-	//msa_u8 *arr_add2=(msa_u8 *)"put your val ";
-	//msa_u8 *got_arr;
+	usart_init(&usart_1,B_SENDER_N_RECEIVER,ONE_STP_BIT,NO_PARITY,EIGHT,POLLING_DRIVEN,FALLING_EDGE); 
+    msa_u8 got_val;
+	msa_u8 temp_val=' ';
+	msa_u8 *arr_add=(msa_u8 *)"you gave me this =>";
+	msa_u8 *arr_add2=(msa_u8 *)"put your val ";
+	msa_u8 *got_arr;
 	//usart_set_isr_TXC_callback(&usart_1,usart_send);
-	//msa_u8 arr[10];
-	usart_set_isr_TXC_callback(&usart_1,usart_send);
+	msa_u8 arr[10];
+	//usart_set_isr_TXC_callback(&usart_1,usart_send);
 	while (1) 
     {
 		
-		//usart_receive_arr(&usart_1,arr,10);
-		//usart_send_arr(&usart_1,arr);
-		
-		PORTA=0xff;
+		usart_receive_arr(&usart_1,arr,10);
+ 		usart_send_arr(&usart_1,arr);
+ 		//usart_send_byte(&usart_1,(msa_u8 *)&val);
+		usart_send_arr(&usart_1,arr_add2);		
+		usart_receive_byte(&usart_1,&got_val);
 		_delay_ms(500);
-		PORTA=0x55;
-		_delay_ms(500);
-		//usart_send_byte(&usart_1,(msa_u8 *)&val);
-		//usart_send_arr(&usart_1,arr_add2);		
-		//usart_receive_byte(&usart_1,&got_val);
-		//_delay_ms(500);
-		//usart_send_arr(&usart_1,arr_add);
-		//usart_send_byte(&usart_1,&got_val);
-		//usart_send_byte(&usart_1,&temp_val);
+		usart_send_arr(&usart_1,arr_add);
+		usart_send_byte(&usart_1,&got_val);
+		usart_send_byte(&usart_1,&temp_val);
 		//usart_receive_arr(&usart_1,got_arr);
-		//usart_send_arr(&usart_1,arr_add);
-		//usart_send_arr(&usart_1,got_arr);
+		usart_send_arr(&usart_1,arr_add);
+		usart_send_arr(&usart_1,got_arr);
     }
 }
 
