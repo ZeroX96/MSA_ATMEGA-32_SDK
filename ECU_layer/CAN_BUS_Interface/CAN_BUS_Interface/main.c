03c188cb0bf6f95c2509a040469ca9624b7039d1
Mahmoud_Saad <msammamsa10@gmail.com>
2019-01-25 10:29:54 +0200

    CAN_init commit

---
+++ ECU_layer/CAN_BUS_Interface/CAN_BUS_Interface/main.c
@@ -0,0 +1,16 @@
+/*
+ * CAN_BUS_Interface.c
+ *
+ * Created: 25/01/2019 09:54:27
+ * Author : Mahmoud
+ */ 
+#include "includes.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
